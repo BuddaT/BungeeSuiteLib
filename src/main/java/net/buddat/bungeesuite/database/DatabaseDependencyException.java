b4cf8f613539bddb74ea69039f99940ba8ba5507
Taufiq Hoven <taufiq.hoven@gmail.com>
2013-04-20 11:01:54 +1000

    Created pom, added database class.

---
+++ src/main/java/net/buddat/bungeesuite/database/DatabaseDependencyException.java
@@ -0,0 +1,19 @@
+package net.buddat.bungeesuite.database;
+
+/**
+ * Indicates that a database dependency has not been met.
+ */
+public class DatabaseDependencyException extends Exception {
+	/**
+	 * Auto-generated serialVersionUID.
+	 */
+	private static final long serialVersionUID = -1737874811904077160L;
+
+	public DatabaseDependencyException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	public DatabaseDependencyException(String message) {
+		super(message);
+	}
+}
