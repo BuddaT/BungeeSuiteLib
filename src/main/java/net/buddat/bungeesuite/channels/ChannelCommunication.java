baa4ac796bdb3756c82b619c28b4533b83aa98e0
Taufiq Hoven <taufiq.hoven@gmail.com>
2013-04-27 14:10:39 +1000

    Added constants for server/proxy channel names.

---
+++ src/main/java/net/buddat/bungeesuite/channels/ChannelCommunication.java
@@ -0,0 +1,13 @@
+package net.buddat.bungeesuite.channels;
+
+public final class ChannelCommunication {
+	/**
+	 * Name of the channel for proxy->server communication.
+	 */
+	public static final String PROXY_TO_SERVER_CHANNEL = "BungeeSuiteMC";
+
+	/**
+	 * Name of the channel for server->proxy communication.
+	 */
+	public static final String SERVER_TO_PROXY_CHANNEL = "BungeeSuite";
+}
